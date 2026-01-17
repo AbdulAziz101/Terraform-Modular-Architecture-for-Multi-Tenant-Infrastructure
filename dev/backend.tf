3e87fdf70d3ea8fa3b3c1a5bd3120be0e03ea60c
AbdulAziz101 <majid5200@live.com>
2026-01-17 23:50:28 +0530

    removed unnecessary files(providers,backend.tf tf plan and module...)

--- dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-1768659288"
-    key            = "env/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-locks"
-  }
-}
-
